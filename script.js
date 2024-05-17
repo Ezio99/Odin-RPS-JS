ffa10e793ce01c64d97be9fd22b065993e8a0754
Ezio99 <vaibhavritam@gmail.com>
2024-05-17 17:46:45 +0800

    Added UI for RPS

--- script.js
+++ script.js
@@ -27,12 +27,12 @@ function getComputerChoice() {
 }
 
 
-function getHumanChoice() {
-    let choice = prompt("Rock, papers or scissors?", "rock")
-    choice = choice.toLowerCase();
+// function getHumanChoice() {
+//     let choice = prompt("Rock, papers or scissors?", "rock")
+//     choice = choice.toLowerCase();
 
-    return choice;
-}
+//     return choice;
+// }
 
 function determineWinner(humanChoice, computerChoice) {
     let result = ""
@@ -49,43 +49,102 @@ function determineWinner(humanChoice, computerChoice) {
 }
 
 
-function playRound() {
+function playRound(e) {
     const computerChoice = getComputerChoice()
-    const humanChoice = getHumanChoice()
+    const humanChoice = e.target.textContent.toLowerCase()
 
     console.log(`Computer chose ${computerChoice}`)
 
-    let result = determineWinner(humanChoice,computerChoice);
+    let result = determineWinner(humanChoice, computerChoice);
 
     console.log(result)
 
-    if(result.includes("Computer")){
-        computerScore+=1
+    if (result.includes("Computer")) {
+        computerScore += 1
     }
-    else if(result.includes("Human")){
-        humanScore+=1
+    else if (result.includes("Human")) {
+        humanScore += 1
     }
 
-    console.log(`Current scores : You ${humanScore} Computer ${computerScore}`)
+
+    compChoice.textContent = `Computer chose ${computerChoice}`
+
+    resultSpan.textContent = result
+
+
+
+
+    if (humanScore >= 5 || computerScore >= 5) {
+        if (humanScore >= 5) {
+            resultSpan.textContent = "You win the game!!!"
+        }
+        else {
+            resultSpan.textContent = "Computer wins the game!!!"
+        }
+
+        disableChoices(true)
+
+        addResetButton()
+
+
+
+    }
+
+
+    score.textContent = `Current scores : You ${humanScore} Computer ${computerScore}`
+
+
 
     return result
 }
 
-function playgame(){
-    humanScore=0
-    computerScore=0
-    for(let i=1;i<=5;i++){
-        console.log(`Round ${i}`)
-        if(playRound().includes("Tie")){
-            console.log(`Replaying round ${i}` )
-            i--;
-        }
-    }
+function disableChoices(value) {
+    const choiceButtons = choice.children
 
-    console.log(`Final score : You ${humanScore} Computer ${computerScore}`)
+    const childArrray = Array.from(choiceButtons);
+
+    childArrray.forEach(i => {
+        i.disabled = value
+    })
 }
 
-playgame()
+function addResetButton() {
+    const resetButton = document.createElement("button")
+
+    resetButton.textContent = "Reset game"
+
+    resetButton.id = "Reset"
+
+    resetButton.addEventListener("click", resetGame)
+    
+    body.appendChild(resetButton)
+}
+
+function resetGame() {
+
+    humanScore = 0, computerScore = 0
+    compChoice.textContent = ""
+    resultSpan.textContent = "Start Game"
+    score.textContent = ""
+    disableChoices(false)
+    document.querySelector("#Reset").remove()
+}
+
+// function playgame() {
+//     humanScore = 0
+//     computerScore = 0
+
+//     // console.log(`Final score : You ${humanScore} Computer ${computerScore}`)
+// }
+
+const choice = document.querySelector(".Choices")
+const compChoice = document.querySelector("#CompChoice")
+const resultSpan = document.querySelector("#Result")
+const score = document.querySelector("#Score")
+const body = document.querySelector("body")
+
+
+choice.addEventListener("click", playRound)
 
 
 
